e161e34b4fa7088fbb6148a75d1b84be702aeb87
PDHS lab <pdhslab@dsusd.k12.ca.us>
2018-05-07 10:00:56 -0700

    Ramon's commit

--- rsa.py
+++ rsa.py
@@ -1,43 +1,29 @@
 terminate = 0
-
+while terminate != 1:
+	print("Encrypt, Decrypt, or Terminate?")
+	answer=input("----->")
+	if answer == "Terminate":
+		terminate += 1
+		print ("Exiting...")
+	elif answer == "Encrypt":
+		n=
+		e=input("What is your ")
+	elif answer == "Decrypt":
+		print ("What is	")
 
 def encryption(e,n):
-	encrypted_message = ""
-	message = input("Input Message: ")
-	for x in message:
-		numerize = ord(x)
+	print ("what is the message?")
+	message = input="Message:"
+	encrypted_message=""
+	for c in message:
+		numerize = ord(c)
 		encrypt = pow(numerize, e, n)
 		denumerize = chr(encrypt)
 		encrypted_message += denumerize
-	print(encrypted_message)
-	print("")
-	
-
-
+		print (encrypted_message)
+		
 def decryption(d,n):
-	decrypted_message = ""
-	encryptedmessage = input("Input encrypted message: ")
-	for t in encryptedmessage:
-		numerize = ord(t)
-		decrypt = pow(numerize, d, n)
-		denumerize = chr(decrypt)
-		decrypted_message += denumerize
-	print(decrypted_message)
-	print("")
-	
-while terminate != 1:
-	print("What would you like to do?")
-	print("---------------Encrypt---------------")
-	print("---------------Decrypt---------------")
-	print("---------------Exit---------------")
-	choice = input("---------------> ")
-	if choice == "Encrypt":
-		e = int(input("Enter your 'e' variable: "))
-		n = int(input("Enter your 'n' variable: "))
-		encryption(e,n)
-	if choice == "Decrypt":
-		d = int(input("Enter your 'd' variable: "))
-		n = int(input("Enter your 'n' variable: "))
-		decryption(d,n)
-	if choice == 'Exit':
-			break
+  for u in encrypted_message:
+  	numerize = ord(u)
+  	decrypt = pow(numerize, d, n)
+  print (decrypt)
